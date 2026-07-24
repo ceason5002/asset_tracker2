b8a0c1b51401fb57ea0b2e9f31c304a5d3a3a144
Chris Eason <ceason5002@gmail.com>
2026-07-24 11:18:10 -0700

    Redirect / to the asset list so admin's View site link works

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('assets/', include('assets.urls')),
+    path('', RedirectView.as_view(pattern_name='assets:asset_list')),
 ]
